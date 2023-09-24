b8ac3a5c970e04996b9e460d0aaa858d7311cb9d
Mohammed Aziz Hammemi <128410730+C0RAZON@users.noreply.github.com>
2023-09-24 12:37:16 +0200

    embeds.py

--- Hannibal Prototype 0 Discord Bot/cogs/embeds.py
+++ Hannibal Prototype 0 Discord Bot/cogs/embeds.py
@@ -7,12 +7,12 @@ class embeds(commands.Cog):
     # help command
     @commands.command()
     async def help_commands(self,ctx):
-        embed = nextcord.Embed(title="Ender Bot Commands",description="Note : command sama3ni t5adm ken Song.mp3 or Song2.mp3 5ater bot mazel under development", color=0x008080)
+        embed = nextcord.Embed(title="Hannibal Bot Commands",description="Note : command sama3ni t5adm ken Song.mp3 or Song2.mp3 5ater bot mazel under development", color=0x008080)
         embed.add_field(name="Audio Commands", value="od5el \no5rej \nsama3ni \nqueue \npause \nkaml \nstop", inline=True)
         embed.add_field(name="Greetings Commands", value="slm \n peace\n chkun_enti \n welcome", inline=True)
-        embed.add_field(name="Admins Commands", value="add_role \n remove_role \n ban \n unban \n kick \nclear", inline=True)
+        embed.add_field(name="Admins Commands", value="add_role \n remove_role \n ban \n unban \n kick \nclear \nmost_active", inline=True)
         await ctx.send(embed=embed)
-    
+
     # Create an Embed (which is not necessary at the moment)
     @commands.command()
     async def chkun_enti(self, ctx):
@@ -27,6 +27,9 @@ class embeds(commands.Cog):
         embed = nextcord.Embed(title=message, color=0x008080)
         await ctx.send(embed=embed)
 
+
+def setup(bot):
+    bot.add_cog(embeds(bot))
 
 def setup(bot):
     bot.add_cog(embeds(bot))
